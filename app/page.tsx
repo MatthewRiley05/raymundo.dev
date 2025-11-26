b2168a1b3795d57b4a1642b1a04cfa78e3645c97
MatthewRiley05 <matthewrileyraymundo@gmail.com>
2025-11-27 00:14:57 +0800

    refactor: simplify Home component by removing unnecessary div wrappers

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,10 @@ import DitherBackground from '@/components/dither_background';
 export default function Home() {
   return (
     <div className="relative w-full h-screen">
-      {/* Dither background - positioned absolutely to cover the entire viewport */}
-      <div className="absolute inset-0" style={{ pointerEvents: 'none' }}>
-        <div style={{ pointerEvents: 'auto' }}>
-          <DitherBackground />
-        </div>
+      <div>
+        <DitherBackground />
       </div>
-
-      {/* Your content goes here */}
-      <div className="relative z-10" style={{ pointerEvents: 'auto' }}>
-        {/* Add your page content */}
+      <div>
       </div>
     </div>
   );
