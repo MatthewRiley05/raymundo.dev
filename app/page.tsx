792892039ce989a7920aa853f1d1bff05c8dc48a
MatthewRiley05 <matthewrileyraymundo@gmail.com>
2025-11-27 00:12:41 +0800

    feat: add dither background component with wave effect and postprocessing

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,19 @@
+import DitherBackground from '@/components/dither_background';
+
 export default function Home() {
   return (
-    <div>
+    <div className="relative w-full h-screen">
+      {/* Dither background - positioned absolutely to cover the entire viewport */}
+      <div className="absolute inset-0" style={{ pointerEvents: 'none' }}>
+        <div style={{ pointerEvents: 'auto' }}>
+          <DitherBackground />
+        </div>
+      </div>
+
+      {/* Your content goes here */}
+      <div className="relative z-10" style={{ pointerEvents: 'auto' }}>
+        {/* Add your page content */}
+      </div>
     </div>
   );
 }
