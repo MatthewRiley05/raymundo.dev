3cd23537974bc70540780d2149e2d086ebdc6b01
MatthewRiley05 <matthewrileyraymundo@gmail.com>
2025-12-23 01:21:09 +0800

    feat: add HomeCardBack component and update CARDS registry

--- components/cards/registry.tsx
+++ components/cards/registry.tsx
@@ -1,9 +1,11 @@
 import type { StackCard } from "../stack/types";
+import HomeCardBack from "./home/HomeCardBack";
 import HomeCardFront from "./home/HomeCardFront";
 
 export const CARDS: StackCard[] = [
     {
         id: "home",
         Front: HomeCardFront,
+        Back: HomeCardBack,
     }
 ]
